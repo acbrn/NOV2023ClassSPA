eaad295af7b9f45f9cca4c6852f35febc88eab90
Andrew Coburn <coburn188@gmail.com>
2023-12-18 18:41:10 -0600

    added afterRender function

--- index.js
+++ index.js
@@ -13,8 +13,14 @@ function render(state = store.Home) {
     ${Footer()}
   `;
   router.updatePageLinks();
+  afterRender();
 }
-
+function afterRender() {
+  // add menu toggle to bars icon in nav bar
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
+}
 router
   .on({
     "/": () => render(),
@@ -29,8 +35,3 @@ router
     }
   })
   .resolve();
-
-// add menu toggle to bars icon in nav bar
-// document.querySelector(".fa-bars").addEventListener("click", () => {
-//   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-// });
